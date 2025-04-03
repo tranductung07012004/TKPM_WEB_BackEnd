99cbeb8ba4ad5b431b7c853850be5e94f206fc9b
tdt-hcmus <122892121+tranductung07012004@users.noreply.github.com>
2025-04-04 02:28:25 +0700

    Delete backend directory

--- backend/src/modules/cloudinary/cloudinary.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CloudinaryProvider } from './cloudinary.provider';
-//import { CloudinaryService } from './cloudinary.service';
-
-@Module({
-  providers: [CloudinaryProvider],
-  exports: [CloudinaryProvider],
-})
-export class CloudinaryModule {}
